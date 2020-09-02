80bbe5eaa3d337b084b2ac9fe6e27e393f87521d
Sanny Do <sanny.do94@gmail.com>
2020-09-02 06:42:11 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,9 +31,9 @@ end
 #   # Return the matrix's content at that row and and column
   
 def matrix_lookup(matrix,row, column)
-  hash = array.to_h => Converts your array to a hash
-hash[key] = value => Get the value associated with the key
-hash.invert[key] = value => 
+  hash = array.to_h => ""
+hash[key] = value => ""
+hash.invert[key] = value => ""
 end
 
  
